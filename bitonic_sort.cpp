527c05d710bf74b64def8ecac318b919df921592
TheDude9506 <165849075+TheDude9506@users.noreply.github.com>
2025-04-23 14:22:26 +0200

    Update bitonic_sort.cpp

--- bitonic_sort.cpp
+++ bitonic_sort.cpp
@@ -6,7 +6,7 @@
 //	MPI_Init(&argc, &argv);
 //	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 //	MPI_Comm_size(MPI_COMM_WORLD, &size);
-//	printf(" fuck off from process %d of %d\n", rank, size);
+//	printf(" process %d of %d\n", rank, size);
 //	MPI_Finalize();
 //	return 0;
 //}
